46408f33cd70ae5178bdf6f62bff858eaa4fa9b2
BHUSHN1611 <bhushanjadhav1603@gmail.com>
2025-08-09 16:24:51 +0530

    vite config upadate

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/railmadatfirstdraft/', // Replace with your actual repo name
+  base: 'railmadatfirstdraft', // Replace with your actual repo name
 })
